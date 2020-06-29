e9d831bb7c084fbd6eea69dcec27be6b4d403bf9
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-06-29 13:46:05 -0700

    Revert to http

--- Backend/node.js
+++ Backend/node.js
@@ -1,41 +1,43 @@
 const ObjectID = require('mongodb').ObjectID;
-const express = require('express');
-const app = express(); // instantiate express
-
-const bodyParser = require("body-parser");//To parse body of post requests
-app.use(bodyParser.json());
-
-
-//For https
-const fs = require('fs');
-const key = fs.readFileSync('../key.pem');
-const cert = fs.readFileSync('../cert.pem');
-const https = require('https');
-
-
-const server = https.createServer({key: key, cert: cert }, app);
-
-
 
 // listen for requests on port 4567
 const port = 4567;
 
+
+const express = require('express');
+var fs = require('fs')
+var https = require('https')
+
+var app = express()
 app.use(require("cors")()) // allow Cross-domain requests 
 app.use(require('body-parser').json()) // When someone sends something to the server, we can recieve it in JSON format
 
+const bodyParser = require("body-parser");//To parse body of post requests
+app.use(bodyParser.json());
+//
+//const server = https.createServer({
+//  key: fs.readFileSync('server.key'),
+//  cert: fs.readFileSync('server.cert')
+//}, app)
+//.listen(port, function () {
+//  console.log('Example app listening on port ' + port);
+//})
+
+
+
+
+
 
 //Require mongodb and import a MongoClient object;
-
 var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017'; //Keep like this
 var databaseName = 'Hackmann';
 var database;
 
-server.listen(port, () => { console.log('listening on ' + port) });
 
-    
 //Connect the client using the url & client
 mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    
     
     database = client.db(databaseName);
     
@@ -87,21 +89,23 @@ mongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     
     var collection = database.collection('Users');
     
-   //Get user
-  app.get("/:user", (req, res) => {
-    // Search for user
-    collection.find({ username: req.params.user })
-    .toArray((err, result) => {
-      if (err) {
-        res.send("Error in GET req.");
-      } else {
-        // if all works
-        res.send(result); // Send users with matching username
-      }
+    //Get user
+    app.get("/:user", (req, res) => {
+        // Search for user
+        collection.find({ username: req.params.user })
+        .toArray((err, result) => {
+        if (err) {
+            res.send("Error in GET req.");
+        } else {
+            // if all works
+            res.send(result); // Send users with matching username
+        }
+        });
     });
+    
+  // listen for requests
+  var listener = app.listen(port, () => {
+    console.log("Your app is listening on port " + listener.address().port);
   });
-//  // listen for requests
-//  var listener = app.listen(port, () => {
-//    console.log("Your app is listening on port " + listener.address().port);
-//  });
 });
+server.listen(port, () => { console.log('listening on ' + port) });
--- Volunteer/main.js
+++ Volunteer/main.js
@@ -1,4 +1,4 @@
-var getURL = 'https://34.74.78.120:4567/'; 
+var getURL = 'http://34.74.78.120:4567/'; 
 
 
 class Card {
