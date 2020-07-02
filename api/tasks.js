b27efdb9e649ff71af10e6661c1e323468fb7284
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-07-02 00:53:42 -0700

    Add back vercel stuff.

---
+++ api/tasks.js
@@ -0,0 +1,26 @@
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+
+let cacheDB = null;
+
+async function connectDatabase(uri) {
+    
+    if(cacheDB) {
+        return cacheDB;
+    }
+    
+    var client = await MongoClient.connect(uri);
+    var db = await client.db(url.parse(uri).pathname.substr(1));
+    
+    cacheDB = db;
+    return db;
+}
+
+module.exports = async (req, res) => {
+    var myDb = await connectDatabase(process.env.MONGODB_URI);
+    var myCollection = await myDb.collection("Tasks");
+    
+    var tasks = await myCollection.find({ }).toArray();
+    res.status(200).json({ tasks });
+}
+
---
+++ api/tutorial.js
@@ -0,0 +1,37 @@
+const url = require('url')
+const MongoClient = require('mongodb').MongoClient
+
+
+let cachedDb = null
+
+
+async function connectToDatabase(uri) {
+
+  if (cachedDb) {
+    return cachedDb
+  }
+
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+  const db = await client.db(url.parse(uri).pathname.substr(1))
+
+
+  cachedDb = db
+  return db
+}
+  
+
+module.exports = async (req, res) => {
+ 
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+
+
+  const collection = await db.collection('Users')
+
+
+  const users = await collection.find({}).toArray()
+
+
+  res.status(200).json({ users })
+}
