e676298524d8b6d024fc56c02c41219631328eed
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-07-02 01:20:59 -0700

    Try to get post requests working on vercel.

---
+++ api/post-tasks.js
@@ -0,0 +1,26 @@
+const url = require("url");
+const MongoClient = require("mongodb").MongoClient;
+
+let cacheDB = null;
+
+async function connectToDatabase(uri) {
+    if(cacheDB) {
+        return cacheDB;
+    }
+    
+    const client = await MongoClient.connect(uri);
+    const db = await client.db(url.parse(uri).pathname.substr(1));
+    cacheDB = db;
+    return db;
+}
+
+module.exports = (req, res) => {
+    var myDB = await connectDatabase(process.env.MONGODB_URI);
+    var myCollection = await myDB.collections("Tasks");
+    
+    myCollection.insertOne(req.body, (err, result) => {
+        if(err) return console.log(err);
+        
+        res.json(result);
+    })
+}
---
+++ api/post-users.js
@@ -0,0 +1,25 @@
+const url = require("url");
+const MongoClient = require("mongodb").MongoClient;
+
+let cacheDB = null;
+
+async function connectToDatabase(uri) {
+    if(cacheDB) {
+        return cacheDB;
+    }
+    
+    const client = await MongoClient.connect(uri);
+    const db = await client.db(url.parse(uri).pathname.substr(1));
+    
+    cacheDB = db;
+    return db;
+}
+
+module.exports = (req, res) => {
+    var myDB = connectDatabase(process.env.MONGODB_URI);
+    var myCollection = myDB.collection("Users");
+    myCollection.insertOne(req.body, (err, result) => {
+        if(err) return console.log(err);
+        res.json(result);
+    })
+}
